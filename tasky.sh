6c4f6cbdb54239d4adacdb305cd0be189a5ca473
David McPherson <154096764+McPsGitHub@users.noreply.github.com>
2024-08-17 16:34:23 -0400

    changing code to run locally

---
+++ tasky.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export REGISTRY="McPsGitHub"
+export REPOSITORY="todo-on-minikube"
+export IMAGE_TAG="lastest"
+
+# Clone the application repostry to the local machine
+git clone https://github.com/mcpsgithub/todo-on-minikube.git
+
+# Build webserver docker image locally 
+docker build -t $REGISTRY/$REPOSITORY:$IMAGE_TAG .
+
+# Run webserver docker container
+run: docker run $REGISTRY/${REPO_FULL_NAME,,}:$IMAGE_TAG
+
+# name: Update MongoDB config and deploy MongoDB   
+kubectl apply -f mongodb-secret.yaml
+kubectl apply -f mongodb-config.yaml          
+kubectl apply -f mongodb.yaml
+
+# name: Deploy webapp
+kubectl apply -f webapp.yaml
